76aeaeaef095ff764a8ef39069a55c09f4ee937d
jonathan xerri <jxerri145@gmail.com>
2019-01-17 09:12:50 +0100

    refactor(UserController): remove useless `@ApiResponse`

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -5,15 +5,11 @@ import { UserService } from "./user.service";
 @ApiUseTags("User")
 @Controller("user")
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get(":id")
   @ApiResponse({
     status: HttpStatus.OK,
-    description: "User trouvé et retourné"
-  })
-  @ApiResponse({
-    status: HttpStatus.NO_CONTENT,
     description: "User trouvé et retourné"
   })
   async getById(@Param("id") id: string) {

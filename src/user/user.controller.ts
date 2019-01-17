f8851a05cc353860da63a36c567585cd55f3aa81
jonathan xerri <jxerri145@gmail.com>
2019-01-17 09:02:13 +0100

    initial commit

---
+++ src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, HttpStatus, Param } from "@nestjs/common";
+import { ApiResponse, ApiUseTags } from "@nestjs/swagger";
+import { UserService } from "./user.service";
+
+@ApiUseTags("User")
+@Controller("user")
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(":id")
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "User trouvé et retourné"
+  })
+  @ApiResponse({
+    status: HttpStatus.NO_CONTENT,
+    description: "User trouvé et retourné"
+  })
+  async getById(@Param("id") id: string) {
+    return this.userService.getById(id);
+  }
+}
